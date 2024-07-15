2771bf5428527c8c0692ee5f7d7326cbf98acbfc
Arslon004 <arslonbozorov2109@gmail.com>
2024-07-15 13:08:36 +0300

    add loginSchema

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,35 +1,21 @@
 
 const LoginPage = () => {
   return (
-    <div className="d-flex justify-content-center align-items-center ">
+    <div className="min-vh-100 d-flex justify-content-center align-items-center ">
       <form className="container mt-4 w-25">
-        <div className="form-group mb-3">
-          <label htmlFor="name">Name</label>
-          <input type="text" id="name" name="name" className="form-control" />
-        </div>
 
         <div className="form-group mb-3">
-          <label htmlFor="age">Age</label>
-          <input type="number" id="age" name="age" className="form-control" />
-        </div>
-
-        <div className="form-group mb-3">
-          <label htmlFor="email">Email</label>
+          <label className="mb-2" htmlFor="email">Email</label>
           <input id="email" name="email" className="form-control" />
         </div>
 
         <div className="form-group mb-3">
-          <label htmlFor="website">Website</label>
-          <input id="website" name="website" className="form-control" />
+          <label className="mb-2" htmlFor="password">Password</label>
+          <input id="password" name="password" className="form-control" />
         </div>
 
         <div className="form-group mb-3">
-          <label htmlFor="date">Created at</label>
-          <input type="date" id="date" name="date" className="form-control" />
-        </div>
-
-        <div className="form-group mb-3">
-          <button type="submit" className="btn btn-primary">
+          <button type="submit" className="btn btn-primary w-100">
             Send
           </button>
         </div>
---
+++ src/schemas/LoginSchema.js
@@ -0,0 +1,8 @@
+import * as yup from 'yup';
+
+let loginSchema = yup.object({
+  email: yup.string().email('Please enter a valid email address').required('Email is required'),
+  password: yup.string().required('Password is required').min(6, 'Password must be at least 6 characters long').max(12, 'Password must not be more than 12 characters long')
+});
+
+export default loginSchema;
